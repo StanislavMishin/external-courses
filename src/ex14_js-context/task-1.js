00628e461ff7cf1ae34ccdecd61c90bb09dd9532
StanislavMishin <mishin.stanislaw@outlook.com>
2021-12-14 14:07:47 +0300

    fix bug

--- src/ex14_js-context/task-1.js
+++ src/ex14_js-context/task-1.js
@@ -1,5 +1,5 @@
-const calculate = {
-  result: 0,
+class Calculator {
+  result = 0;
 
   initialState(num) {
     if (Number.isFinite(num)) {
@@ -7,46 +7,54 @@ const calculate = {
     }
 
     return this;
-  },
+  }
+
   add(num) {
     if (Number.isFinite(num)) {
       this.result += num;
     }
 
     return this;
-  },
+  }
+
   subtract(num) {
     if (Number.isFinite(num)) {
       this.result -= num;
     }
 
     return this;
-  },
+  }
+
   divide(num) {
     if (Number.isFinite(num)) {
       this.result /= num;
     }
 
     return this;
-  },
+  }
+
   multiply(num) {
     if (Number.isFinite(num)) {
       this.result *= num;
     }
 
     return this;
-  },
+  }
+
   getResult() {
     return this.result;
-  },
+  }
+
   reset() {
     this.result = 0;
 
     return this;
-  },
+  }
+
   fetchData(callback) {
     setTimeout(() => callback(500), 1000);
-  },
-};
+    return this;
+  }
+}
 
-module.exports = calculate;
+module.exports = Calculator;
